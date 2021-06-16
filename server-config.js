6ee0af9327b0dd882211d36cad2e687961112560
Emily Gleeson <engleeson@gmail.com>
2021-06-16 14:46:07 -0400

    front end mostly built, started work on back end

---
+++ server-config.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const tables = [
+    {
+        id: "GoldenGod",
+        name: "Dennis Reynolds",
+        phone: 555-555-5555,
+        email: "dreynolds@philly.edu",
+    }
+];
+const waitList = [
+    {
+        id: "DayMan",
+        name: "Charlie Kelly",
+        phone: 555-555-5656,
+        email: "ratking@ghgh.com",
+    }
+];
+
+//paths for three html files
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/front-end/index.html')));
+app.get('/book-table', (req, res) => res.sendFile(path.join(__dirname, '/front-end/book-table.html')));
+app.get('/reserved', (req, res) => res.sendFile(path.join(__dirname, '/front-end/reserved.html')));
+
+app.get('/api/book-table', (req, res) => res.json(tables));
+app.get('/api/reserved', (req, res) => res.json(waitList));
+
+app.post('/api/book-table', (req, res) => {
+    const bookings = req.body;
+    console.log(bookings);
+
+    tables.push(bookings);
+    console.log(bookings);
+
+}
+
+)
+
+
+app.listen (PORT, () => console.log(`Listening on ${PORT}`));
